4575fc3732b854051d2cff1f2c8834c3082d7c16
Gruak <43846312+Gruak@users.noreply.github.com>
2023-09-13 21:31:38 +0200

    Refactor javascript worklets

--- src/hook.tsx
+++ src/hook.tsx
@@ -3,34 +3,20 @@ import type { FrameProcessor } from 'react-native-vision-camera';
 import { Worklets } from 'react-native-worklets-core';
 import { useState } from 'react';
 
-import {
-  BarcodeFormat,
-  DefaultCodeScannerOptions,
-  scanBarcodes,
-} from './index';
+import { BarcodeFormat, scanBarcodes } from './index';
 import type { Barcode, CodeScannerOptions } from './index';
 
 export function useScanBarcodes(
   types: BarcodeFormat[],
-  options: CodeScannerOptions = DefaultCodeScannerOptions
+  options?: CodeScannerOptions
 ): [FrameProcessor, Barcode[]] {
   const [barcodes, setBarcodes] = useState<Barcode[]>([]);
   const setBarcodesJS = Worklets.createRunInJsFn(setBarcodes);
 
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet';
-    // For some reason, arguments to hooks accessed from worklets don't have the same runtime type they
-    // are declared as. This leads to tricky errors, hence the Array.from and reduce on the options' keys.
-    //
-    // Example error:
-    // Frame Processor threw an error: Exception in HostFunction: Received an unknown HostObject! Cannot convert to a JNI value.
-    const opts = Object.keys(options || {}).reduce((acc, key) => {
-      acc[key as keyof CodeScannerOptions] = (options || {})[
-        key as keyof CodeScannerOptions
-      ];
-      return acc;
-    }, {} as CodeScannerOptions);
-    const detectedBarcodes = scanBarcodes(frame, Array.from(types), opts);
+
+    const detectedBarcodes = scanBarcodes(frame, types, options);
     setBarcodesJS(detectedBarcodes);
   }, []);
 

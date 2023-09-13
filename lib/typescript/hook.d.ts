d4ee848370be08a9697d15da964a78818cfef9ca
Gruak <43846312+Gruak@users.noreply.github.com>
2023-09-13 15:25:02 +0200

    Build js bundle

---
+++ lib/typescript/hook.d.ts
@@ -0,0 +1,5 @@
+import type { FrameProcessor } from 'react-native-vision-camera';
+import { BarcodeFormat } from './index';
+import type { Barcode, CodeScannerOptions } from './index';
+export declare function useScanBarcodes(types: BarcodeFormat[], options?: CodeScannerOptions): [FrameProcessor, Barcode[]];
+//# sourceMappingURL=hook.d.ts.map
